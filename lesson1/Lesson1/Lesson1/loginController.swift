48922bc095caea53997198c9248c23a29bf788ad
EgorkaJAred <egorkajared@gmail.com>
2020-06-12 21:16:46 +0300

    форма

---
+++ lesson1/Lesson1/Lesson1/loginController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginController.swift
+//  Lesson1
+//
+//  Created by Егор Ненастьев on 12.06.2020.
+//  Copyright © 2020 Егор Ненастьев. All rights reserved.
+//
+
+import UIKit
+
+class loginController: NSObject {
+
+}
