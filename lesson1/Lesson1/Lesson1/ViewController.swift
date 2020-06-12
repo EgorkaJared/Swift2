3658ae15578fb6678d0c86b6a1565404243aab87
EgorkaJAred <egorkajared@gmail.com>
2020-06-12 19:54:23 +0300

    структура

---
+++ lesson1/Lesson1/Lesson1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson1
+//
+//  Created by Егор Ненастьев on 11.06.2020.
+//  Copyright © 2020 Егор Ненастьев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
