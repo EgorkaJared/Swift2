f8de175db5c6c42fe87aaf4b36ba4fbb4026f060
EgorkaJAred <egorkajared@gmail.com>
2020-06-20 16:29:16 +0300

    форма групп

---
+++ lesson1/Lesson1/Lesson1/Model/oneGroup.swift
@@ -0,0 +1,22 @@
+//
+//  oneGroop.swift
+//  Lesson1
+//
+//  Created by Егор Ненастьев on 20.06.2020.
+//  Copyright © 2020 Егор Ненастьев. All rights reserved.
+//
+
+import UIKit
+
+class oneGroup {
+    
+    var groupName: String
+    //var photo: UIImage
+  
+    init(groupName:String) {
+        self.groupName = groupName
+        
+        //self.photo = photo
+        }
+    }
+    
---
+++ lesson1/Lesson1/Lesson1/TableCellGroup.swift
@@ -0,0 +1,30 @@
+//
+//  TableCellGroup.swift
+//  Lesson1
+//
+//  Created by Егор Ненастьев on 20.06.2020.
+//  Copyright © 2020 Егор Ненастьев. All rights reserved.
+//
+
+import UIKit
+
+class TableCellGroup: UITableViewCell {
+    
+    @IBOutlet weak var groupName: UILabel!
+    
+    @IBOutlet weak var groupPhoto: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
