c904915102c9dda5020aecd7d4bc265826e533eb
EgorkaJAred <egorkajared@gmail.com>
2020-06-20 14:45:16 +0300

    привязка класса друзья

---
+++ lesson1/Lesson1/Lesson1/TableCellFriend.swift
@@ -0,0 +1,24 @@
+//
+//  TableCellFriend.swift
+//  Lesson1
+//
+//  Created by Егор Ненастьев on 20.06.2020.
+//  Copyright © 2020 Егор Ненастьев. All rights reserved.
+//
+
+import UIKit
+
+class TableCellFriend: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
